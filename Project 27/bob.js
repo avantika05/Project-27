3baa11aee6159fe8f9c6f9b797518ed47effa29e
avantika05 <73350877+avantika05@users.noreply.github.com>
2021-01-15 02:50:45 -0500

    Add files via upload

---
+++ Project 27/bob.js
@@ -0,0 +1,9 @@
+class Bob extends BaseClass{
+  constructor(x,y,r) {
+    super(x,y,70)
+    this.image = loadImage("sprites/paper.png");
+  }
+  display(){
+
+  }
+}
---
+++ Project 27/sketch.js
@@ -0,0 +1,54 @@
+
+const Engine = Matter.Engine;
+const World = Matter.World;
+const Bodies = Matter.Bodies;
+const Body = Matter.Body;
+
+function preload()
+{
+	
+}
+
+function setup() {
+	createCanvas(800, 700);
+
+
+	engine = Engine.create();
+	world = engine.world;
+
+bobObject1 = new Bob(200,200,70,70);
+bobObject2 = new Bob(300,200,70,70);
+bobObject3 = new Bob(400,200,70,70);
+bobObject4 = new Bob(500,200,70,70);
+bobObject5 = new Bob(600,200,70,70);
+
+rope1 = new Rope(bobObject1.body,roofObject.body,-bobDiameter*2,0);
+rope2 = new Rope(bobObject2.body,roofObject.body,-bobDiameter*2,0);
+rope3 = new Rope(bobObject3.body,roofObject.body,-bobDiameter*2,0);
+rope4 = new Rope(bobObject4.body,roofObject.body,-bobDiameter*2,0);
+rope5 = new Rope(bobObject5.body,roofObject.body,-bobDiameter*2,0);
+
+	Engine.run(engine);
+  
+}
+
+
+function draw() {
+  rectMode(CENTER);
+  background(0);
+  
+bobObject1.display();
+bobObject2.display();
+bobObject3.display();
+bobObject4.display();
+bobObject5.display();
+
+rope1.display();
+rope2.display();
+rope3.display();
+rope4.display();
+rope5.display();
+
+  drawSprites();
+ 
+}
